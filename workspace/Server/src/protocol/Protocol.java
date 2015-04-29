d72d51b2d737910a51ddc731de67dfd6ea49f655
James Nolan <james.nolan@heig-vd.ch>
2015-04-29 17:51:44 +0200

    Ajout du package spécifique au protocol. Début d'implémentation de la classe Protocol (qui contient principalement les constantes des commandes disponibles)

---
+++ workspace/Server/src/protocol/Protocol.java
@@ -0,0 +1,10 @@
+package protocol;
+
+public class Protocol {
+	public final static String CMD_AUTH = "AUTH";
+	public final static String CMD_REGISTER = "REGISTER";
+	public final static String CMD_REFRESH = "REFRESH";
+	public final static String CMD_CREATE = "AUTH";
+	public final static String CMD_JOIN = "REGISTER";
+	public final static String CMD_QUIT = "REFRESH";
+}
